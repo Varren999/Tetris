fcaaf051915882b080816231cae7823b1dc13f12
Varren999 <varren999@gmail.com>
2024-02-13 10:48:50 +0700

    add figure

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -10,6 +10,65 @@ namespace Tetris
     {
         static void Main()
         {
+            bool[,] bools = new bool[15, 10];
+
+            string[,] pole = {{"#                    ##########" },
+                              {"#                    #        #" },
+                              {"#                    #        #" },
+                              {"#                    #        #" },
+                              {"#                    #        #" },
+                              {"#                    ##########" },
+                              {"#                    # Score: #" },
+                              {"#                    #        #" },
+                              {"#                    #        #" },
+                              {"#                    #        #" },
+                              {"#                    #        #" },
+                              {"#                    #        #" },
+                              {"#                    #        #" },
+                              {"#                    #        #" },
+                              {"#                    #        #" },
+                              {"###############################" } };
+
+            string[,] figureA = { { "[][]" },
+                                  { "[][]" }};
+
+            string[] figureB = { "[][][][]" };
+
+            string[,] figureC = { { "[][]  " },
+                                  { "  [][]" } };
+
+            string[,] figureD = { {"[][][]"},
+                                  {"[]    "} };
+
+            string[,] figureE = { {"[]"},
+                                  {"[]"},
+                                  {"[]"},
+                                  {"[]"}};
+
+            string[,] figureF = { { "  []"},
+                                  { "[][]"},
+                                  { "[]  "} };
+
+            string[,] figureG = { { "[][]"},
+                                  { "  []"},
+                                  { "  []"} };
+
+            string[,] figureH = { { "  []  "},
+                                  { "[][][]"} };
+            try
+            {
+                for (int c = 0; c < pole.Rank; c++)
+                {
+                    for (int i = 0; i < pole.Length; i++)
+                    {
+                        Console.WriteLine(pole[i, c]);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
